bbe52d9acf3998b6cbdd9fb436bfe4c6c29979b7
niconan <nick.hax@gmail.com>
2015-04-17 18:38:43 -0700

    finish

--- linkedlist.rb
+++ linkedlist.rb
@@ -8,9 +8,20 @@ class LinkedList
     @size = 1
   end
 
-  def read(index)
+  def read_steps(index)
     node = crawl(index, true)
     puts "#{node.word}: #{node.definition}"
+  end
+
+  def read_at(index)
+    node = crawl(index)
+    puts "#{node.word}: #{node.definition}"
+  end
+
+  def read_entire
+    0.upto(@size - 1) do |index|
+      read_at(index)
+    end
   end
 
   def add_to_end(word, definition)
@@ -32,12 +43,27 @@ class LinkedList
     @size += 1
   end
 
+  def search(word, index)
+    current_node = @head
+    total_traversed = 0
+    until current_node.word == word || current_node.next == nil
+      current_node = current_node.next
+      total_traversed += 1
+    end
+    if current_node.word == word
+      puts "Word '#{word}' found with definition: '#{current_node.definition}' in bucket #{index}."
+      puts "It took #{total_traversed} traversals."
+      exit
+    else
+      puts "Word not found in bucket #{index}."
+    end
+  end
+
   def crawl(index, prints = false)
     crawler = @head
     if prints
       index.times do
         puts "Crawler at: #{crawler.word}"
-        crawler = crawler.next
       end
     else
       index.times { crawler = crawler.next }
