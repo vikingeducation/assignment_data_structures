1a5bd13856600fbc474719d148e52263c1986ec7
Sia <siakaramalegos@gmail.com>
2015-11-28 13:38:32 -0600

    Finishes hash table

--- linked_list.rb
+++ linked_list.rb
@@ -8,13 +8,13 @@ class LinkedList
     @last = first_node
   end
 
-  def add_first_node(word, definition)
+  def add_first_node(word, definition = nil)
     @head = Node.new(word, definition, nil)
     @last = @head
   end
 
   # O(1)
-  def add_node_to_end(word, definition)
+  def add_node_to_end(word, definition = nil)
     if @head.nil?
       add_first_node(word, definition)
     else
@@ -22,16 +22,16 @@ class LinkedList
       @last.next = new_node
       @last = new_node
     end
-    puts "Added word '#{word}' with definition '#{definition}'"
+    # puts "Added word '#{word}' with definition '#{definition}'"
   end
 
   # O(n) since must crawl through each item
-  def add_node_at_index(word, definition, index)
+  def add_node_at_index(word, definition = nil, index)
     counter = 0
     current_node = @head
     prev_node = nil
 
-    # crawl to index
+    # crawl to index to set both prev_node and current_node
     while counter < index
       prev_node = current_node
       current_node = current_node.next
@@ -43,7 +43,7 @@ class LinkedList
     prev_node.next = new_node
     new_node.next = current_node
 
-    puts "Added '#{word}: #{definition}' at index #{index}."
+    # puts "Added '#{word}: #{definition}' at index #{index}."
   end
 
   # No error handling
@@ -57,8 +57,26 @@ class LinkedList
       counter += 1
     end
 
-    puts "Node at index #{index} is '#{current_node.word}: #{current_node.definition}', and took #{counter+1} steps."
+    # puts "Node at index #{index} is '#{current_node.word}: #{current_node.definition}', and took #{counter+1} steps."
     current_node
+  end
+
+  def get_definition(lookup_word)
+    counter = 0
+    size = self.length
+    current_node = @head
+
+    while counter < size
+      if current_node.word == lookup_word
+        puts "#{lookup_word}: #{current_node.definition}, #{counter} steps"
+        return current_node.definition
+      else
+        current_node = current_node.next
+        counter += 1
+      end
+    end
+
+    puts "I'm sorry, didn't find '#{lookup_word}' in the current dictionary. #{counter} steps."
   end
 
   # O(n^2) because loop in a loop.  It is "in place" - just resets the next pointer to the reverse object.
@@ -89,17 +107,17 @@ class LinkedList
       current_node = current_node.next
       counter += 1
     end
-    puts "Length is #{counter}."
+    # puts "Length is #{counter}."
     counter
   end
 end
 
-x = LinkedList.new
-x.add_node_to_end('hi', 'short for hello')
-x.add_node_to_end('bye', 'short for goodbye')
-x.add_node_to_end('hola', 'Spanish for hello')
-x.find_node(1)
-x.find_node(2)
-x.add_node_at_index('geia sou', "Greek for hello", 1)
-x.length
-x.reverse_in_place
+# x = LinkedList.new
+# x.add_node_to_end('hi', 'short for hello')
+# x.add_node_to_end('bye', 'short for goodbye')
+# x.add_node_to_end('hola', 'Spanish for hello')
+# x.find_node(1)
+# x.find_node(2)
+# x.add_node_at_index('geia sou', "Greek for hello", 1)
+# x.length
+# x.reverse_in_place
