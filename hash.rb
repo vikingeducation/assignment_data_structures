40455c7d90991d4b96107e9afa419ce7d36ce279
Adrian Mui <adrian.mui@gmail.com>
2016-07-13 19:32:33 -0400

    hash.rb

--- hash.rb
+++ hash.rb
@@ -1,3 +1,5 @@
+require_relative 'linked_list'
+
 class HashTable
  
   def initialize
@@ -5,12 +7,33 @@ class HashTable
   end
 
 
-  def hash
-    
+  def hash(word)
+    word[0].downcase.ord - 97
   end
 
-  def insert
-
+  def insert(word)
+    if @buckets[hash(word)] == nil
+      new_list = LinkedList.new
+      new_list.add_node(word)
+      @buckets[hash(word)] = new_list
+    else
+      @buckets[hash(word)].add_node(word)
+    end
   end
+
+  def render_list
+    @buckets.each_with_index do |item, index|
+      print "item #{index} with size #{item.get_size}"
+    end
+  end
+
+  def define(word)
+    @buckets[hash(word)]
+
+
+
+    else 
+      raise "not found"
+    end
 
 end
--- linked_list.rb
+++ linked_list.rb
@@ -93,9 +93,7 @@ class LinkedList
     
   end
 
-
   def reverse
-    
     #1-> 2 - > 3 -> 4 -> 5
     current = @head
     previous = nil
@@ -107,11 +105,6 @@ class LinkedList
       previous = current #previous value is now set as 'current'. this is because we will change current
       current = next_node #1 no longer points to 2, but we have next_node to hold the node 2!
     end
-
-  
-
-
-
    current = previous
    return current
    
