f0e0bc5890d5a3bab3ea89f37016b60d3be218dd
Hannah Squier <hannahsquier@gmail.com>
2016-07-13 14:30:36 -0700

    added LinkedList#insert

--- hash/lib/linked_list.rb
+++ hash/lib/linked_list.rb
@@ -7,7 +7,36 @@ class LinkedList
   end
 
   def append(new_node)
-
+    if empty?
+      @head = new_node
+      @last = @head
+    else
+      @last.next = new_node
+      @last = new_node
+    end
+    self
   end
 
+  def insert(new_node, index)
+    crawler = 0
+    current_node = @head
+    while crawler <= index
+      
+      if crawler == index
+        pre = current_node
+        post = current_node.next
+        new_node.next = post
+        pre.next = new_node
+      end
+      crawler += 1
+      current_node = current_node.next
+    end
+    self
+  end
+
+  private
+
+  def empty?
+    @head == nil
+  end
 end
--- hash/spec/linked_list_spec.rb
+++ hash/spec/linked_list_spec.rb
@@ -25,15 +25,36 @@ describe LinkedList do
   end
 
   describe '#append' do
+
     it 'changes the pointer of the current last node to the new last node' do
       last_node = ll_with_one_node.last  
       ll_with_one_node.append(node2)
       expect(last_node.next).to eq(node2)
     end
 
+    it 'sets new last node is the new ' do
+      ll_with_one_node.append(node2)
+      expect(ll_with_one_node.last).to eq(node2)
+    end
+
+    it 'sets head and last to node if LL starts off empty' do
+      ll.append(node1)
+      expect(ll.head).to eq(node1)
+      expect(ll.last).to eq(node1)
+    end
   end
 
-  #append
+  describe '#insert' do
+
+    let(:filled_ll) { ll.append(node1).append(node2).append(node1).append(node2) }
+
+    it "" do 
+    end
+
+    # it "sets previous nodes pointer equal to new node" do
+    #   filled_ll.insert(node1)
+    # end
+  end
   #insert
   #read
   #delete 
