4a08fe8ce15b342d720993ed099b5c87754f060e
David Jiang <davidjgrey@gmail>
2016-07-13 14:54:42 -0700

    hash insert method

--- hash_table.rb
+++ hash_table.rb
@@ -1,4 +1,27 @@
-Node = Struct.new(:word,:definition,:next_node)
+Node = Struct.new(:word,:next_node)
+
+class HashTable
+
+def initialize
+  @buckets=[]
+end
+
+def hash(word)
+#takes a word and returns an array index based on its first letter
+  word[0].downcase.ord-97
+end
+
+def insert(word)
+  index=hash(word)
+  if @buckets[index]
+    @buckets[index].add_node(word)
+  else
+    @buckets[index]=LinkedList.new
+    @buckets[index].add_node(word)
+  end
+end
+
+end
 
 class LinkedList
   attr_reader :first, :last
@@ -8,12 +31,12 @@ class LinkedList
     @last=nil
   end
 
-  def add_node(word,definition)
+  def add_node(word)
     if @first.nil?
-      @first=Node.new(word,definition,nil)
+      @first=Node.new(word, nil)
       @last=@first
     else
-      new_node=Node.new(word,definition,nil)
+      new_node=Node.new(word,nil)
       @last.next_node=new_node
       @last=new_node
     end
@@ -37,16 +60,15 @@ class LinkedList
       end
 
       puts "Searched #{counter+1} nodes."
-      puts "Node at #{index} is #{current_node.word}: #{current_node.definition}"
       current_node
 
   end
 
   #The method below uses O(n) to insert a new node
-  def insert_node(index, word, definition)
+  def insert_node(index, word)
     node_before_index = find_node(index - 1)
     node_at_current_index = find_node(index)
-    new_node = Node.new(word, definition,node_at_current_index)
+    new_node = Node.new(word,node_at_current_index)
     node_before_index.next_node = new_node
   end
 
