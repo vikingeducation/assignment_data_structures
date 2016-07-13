0b8bfcb483ea6f747cb0198099f449c374149e10
cjvirtucio87 <cjv287@gmail.com>
2016-07-13 09:59:49 -0700

    Finished queue.

--- stacks_queues.rb
+++ stacks_queues.rb
@@ -1,6 +1,7 @@
 module DataStructuresAssignment
   
   class Stack
+    attr_reader :data
 
     def initialize
       @data = []
@@ -22,11 +23,40 @@ module DataStructuresAssignment
     end
 
     def peek
-      @data[-1] unless empty?
+      raise if empty?
+      @data[-1]
     end
 
   end
 
   class Queues
+
+    def initialize
+      @data = []
+    end
+
+    def empty?
+      @data.length == 0
+    end
+
+    def enqueue(item)
+      return @data = [item] if empty?
+      buffer = [item]
+      @data = buffer + @data
+    end
+
+    def dequeue
+      raise if empty?
+      buffer = @data[0]
+      @data.delete_at(0)
+      buffer
+    end
+
+    def peek
+      raise if empty?
+      @data[0]
+    end
+
+  end
 
 end
