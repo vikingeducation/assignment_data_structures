4b5a305cdd1715b4276af8db768301bb5e97ce14
essian <jessicampdennis@hotmail.com>
2016-07-13 19:22:29 +0100

    adds code to linked list class

--- linked_list.rb
+++ linked_list.rb
@@ -3,40 +3,63 @@ Node = Struct.new :data, :next
 
 class LinkedList
 
-attr_reader :head, :last
+  attr_reader :head, :last
 
-def initialize(first=nil)
-  @head = first
-  @last = first
-end
-
-def add_first_node(data)
-  @head = Node.new(data, nil)
-  @last = @head
-end
-
-
-def read_node(index)
-end
-
-def add_node(data)
-  if @head.nil?
-  add_first_node
-  else
-  @last.next = Node.new(data, nil)
-  @last = @last.next
+  def initialize(first=nil)
+    @head = first
+    @last = first
   end
-end
 
-def insert_node(data, index)
-  holder = @head
-  (index - 1).times do
-    holder = holder.next
+  def add_first_node(data)
+    @head = Node.new(data, nil)
+    @last = @head
   end
-  holder = Node.new(data, holder)
-end
+
+
+  def read_node(index)
+  end
+
+  def add_node(data)
+    if @head.nil?
+      add_first_node(data)
+    else
+      @last.next = Node.new(data, nil)
+      @last = @last.next
+    end
+  end
+
+  def insert_node(data, index)
+    # Big O - constant time, independent of number of nodes
+    holder = @head
+    (index - 1).times do
+      holder = holder.next
+    end
+    holder = Node.new(data, holder)
+  end
+
 
   def reverse
-
+    
+    holder = @head
+    until @head.next.nil?
+      until holder.next == @last do
+          holder = holder.next
+      end
+        @last.next = holder
+        holder.next = nil
+    end
+    @last, @head = @head, @last
+    
   end
 end
+
+list = LinkedList.new
+list.add_node('a')
+list.add_node('b')
+list.add_node('c')
+p list.reverse
+
+
+
+[Finished in 0.4s]
+
