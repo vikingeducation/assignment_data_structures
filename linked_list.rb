843bbec24bdd9c1a1fc1eaee2b4dc646ae9b95f4
schoetlr <srlrs20020@gmail.com>
2016-07-13 14:58:35 -0400

    working on reverse

--- linked_list.rb
+++ linked_list.rb
@@ -17,13 +17,42 @@ class LinkedList
     i = 0
     current_node = @head
 
-    while ( i < index)
+    while i < index
       puts "the node is: #{@current_node.data}"
       current_node = current_node.next
       i += 1 
     end
 
     current_node.data
+  end
+
+  def insert_node_at_index(new_node, index)
+    i = 0
+    current_node = @head
+    while i < index - 1
+      current_node = current_node.next
+      i += 1
+    end
+    previous_node = current_node
+    current_node = previous_node.next
+    
+    previous_node.next = new_node
+    new_node.next = current_node
+    
+  end
+
+  def reverse
+    i = 0
+    previous_node = nil
+    current_node = @head
+    loop do
+      
+      previous_node = current_node
+      current_node = current_node.next
+      
+
+    end until current_node.next == nil
+
   end
 
 
