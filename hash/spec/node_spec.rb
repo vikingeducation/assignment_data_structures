0f3c4e4ff6379de350266ae0b4e83870df1cc046
Hannah Squier <hannahsquier@gmail.com>
2016-07-13 11:51:29 -0700

    create pointer reassignment test

--- hash/spec/node_spec.rb
+++ hash/spec/node_spec.rb
@@ -2,7 +2,7 @@
 require 'node'
 
 describe Node do
-  
+  let(:next_next_node) { Node.new(3, nil) }
   let(:next_node){ Node.new(2, nil) }
   let(:node){ Node.new(1, next_node) }
 
@@ -32,10 +32,10 @@ describe Node do
       expect(node.next).to eq(next_node)
     end
 
-    # it 'reassigns data when reset' do
-    #   node.data = 2
-    #   expect(node.data).to eq(2)
-    # end
+    it 'reassigns next when reset' do
+      next_node.next = next_next_node
+      expect(next_node.next).to eq(next_next_node)
+    end
   end
 end
 
