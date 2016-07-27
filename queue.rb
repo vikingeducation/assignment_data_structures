297d11f658e249a78dfe0596b92890c799caf09c
Joe Bernardi <joebernardi@hotmail.com>
2016-07-27 17:50:59 -0500

    adjust dequeue

--- queue.rb
+++ queue.rb
@@ -28,29 +28,41 @@ class Queue
 		# place element at index 0
 		@queue[ index ] = item
 
-
+binding.pry
 	end
 
 
 	def dequeue
 
-		index = @queue.length
+		@dequeue[ @dequeue.length ] = @queue[ @queue.length - 1 ]
 
-		unless empty?
+		@queue[ @queue.length - 1 ] = nil
 
-			until @queue[ index - 1 ] != nil
-
-				index -= 1
-
-			end
-
-			@queue[ index - 1 ] = nil
-
-		end
+		shrink_queue
 
 	end
 
 
+	def shrink_queue
+
+		index = 0
+		arr = []
+
+		until index == @queue.length
+
+			if @queue[ index ] != nil
+
+				arr[ index ] = @queue[ index ]
+
+			end
+
+			index += 1
+
+		end
+
+		@queue = arr
+
+	end
 
 
 
@@ -58,7 +70,7 @@ class Queue
 
 		unless empty?
 
-			return @queue[ @queue.length - 1 ]
+			puts @queue[ @queue.length - 1 ]
 
 		end
 
@@ -73,6 +85,11 @@ class Queue
 	end
 
 
+	def print
+
+		puts @dequeue
+
+	end
 
 
 end #/.queue
@@ -82,6 +99,9 @@ arr = Queue.new
 arr.enqueue( "h" )
 arr.enqueue( "e" )
 arr.enqueue( "l" )
+
+arr.peek
+
 arr.enqueue( "l" )
 arr.enqueue( "o" )
 
@@ -89,4 +109,4 @@ arr.enqueue( "o" )
 	arr.dequeue
 end
 
-arr.peek
+arr.print
