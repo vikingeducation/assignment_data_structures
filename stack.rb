490fbb6a3024c2a3f7584961570824e693a8b67e
Joe Bernardi <joebernardi@hotmail.com>
2016-07-26 19:45:22 -0500

    add push

--- stack.rb
+++ stack.rb
@@ -1,6 +1,54 @@
+require 'pry'
+
 class Stack
+
+	def initialize( stack )
+
+		@stack = stack
+		@pop = nil
+
+
+	end
+
+
+	def push( item )
+
+		@stack[ @stack.length ] = item
+
+	end
+
+
+	def pop
+
+		@pop = @stack[ @stack.length - 1 ]
+
+		return @pop
+
+	end
+
+
+
+
+
+
+	def peek
+
+
+	end
+
+
+	def empty?
+
+
+
+	end
 
 
 
 
 end #/.Stack
+
+
+arr = Stack.new( [1,2,3,4,5,6,7] )
+arr.push( 8 )
+arr.pop
