0a891faf9cc7582393cd4748ef02cdd1425a12b1
Joe Bernardi <joebernardi@hotmail.com>
2016-07-27 22:00:40 -0500

    work on render

--- hashtable.rb
+++ hashtable.rb
@@ -1,160 +1,55 @@
-# Build a linked list node that contains the word and its definition (and anything else you need) using a Struct (not a class).
-# Build a basic linked list class that allows you to:
-	# Create the list
-	# Read nodes at a particular index (add a printout which tracks the crawler's progress or at least returns the number of steps). In the comments, state the Big-O time of this operation.
-	# Insert nodes at a particular index or, separately, at the end of the list (so store a reference to the last node). In the comments, state the Big-O time of this operation.
-
-# Build a method #reverse on your LinkedList class that will entirely flip the order of this list, so the "head" and the "tail" are swapped and all the links point in opposite directions.
+require_relative 'linkedlist'
 
 require 'pry'
 
-Node = Struct.new( :word, :definition, :next )
+class Hashtable
 
-class LinkedList
+	def initialize
 
-	attr_accessor :head, :last
-
-	def initialize( first_node = nil )
-
-		@head = first_node
-		@last = first_node
+		@buckets  = []
+		@list = nil
 
 	end
 
-	def add_first_node( word, definition )
 
-		@head = Node.new( data, definition, nil )
-		@last = @head
+	def hash( word )
+
+		return ( word[ 0 ].downcase.ord - 97 )
 
 	end
 
 
 
-	def add_node( word, definition )
+	def insert( word )
 
-		if @head.nil?
 
-			add_first_node( word, definition )
+		index = hash( word )
+
+		if @buckets[ index ] == nil
+
+			@buckets[ index ] = LinkedList.new( Node.new( word, "it is a #{word}", nil) )
 
 		else
 
-			new_node = Node.new( word, definition )
-
-			@last.next = new_node
-
-			@last = new_node
-
-		end
-
-		puts "Added node with value: #{word}: #{definition}"
-
-	end
-
- # Big O is O(n) since the size of the linked list determines length to traverse the nodes
-	def find_node( index )
-
-
-		count = 0
-		current_node = @head
-
-		while count < index
-
-
-		raise "No node at index" if current_node.next == nil
-
-			print_node( current_node, count )
-
-			current_node = current_node.next
-			count += 1
-
-		end
-
-		puts "Found node at index #{count}"
-		puts "Word is #{current_node.word}"
-		puts "Definition is #{current_node.definition}"
-
-	end
-
-
-	def print_node( node, index )
-
-		puts "Currently at index #{index}: #{node.word}"
-
-	end
-
-
-	# this is O(1) assuming we've already foung the node since we're only moving references
-	def insert_node( word, definition, index )
-
-
-
-
-		count = 0
-		current_node = @head
-		last_node = @head
-
-
-		while count < index
-
-			last_node = current_node
-			current_node = current_node.next
-
-			count += 1
-
-		end
-
-		if last_node.next == nil
-
-			add_node( word, definition )
-
-		else
-
-			new_node = Node.new( word, definition )
-			new_node.next = current_node
-			last_node.next = new_node
-
-			puts "Inserted #{new_node.word} at index: #{count}"
+			@buckets[ index ].add_node( word, "it is a #{word}" )
 
 		end
 
 	end
 
 
-	def remove_node
+	def render_list
 
-		return nil if @head.nil?
+		@buckets.each_with_index do | e, i |
 
-		node = @head
+			unless e.nil?
+binding.pry
+				puts e.head
 
-		@head = @head.next
-
-		return node
-
-	end
-
-
-
-
-	def reverse
-
-		# starting at the first node
-		return if @head.nil?
-
-		temp = self.remove_node
-		temp.next = nil
-		@last = temp
-
-		until @head.nil?
-
-			node = self.remove_node
-			node.next = temp
-			temp = node
+			end
 
 		end
 
-		@head = temp
-			# it's next node should point back to it
-			# the node after a node should be the next to the previous node
 
 	end
 
@@ -162,18 +57,14 @@ class LinkedList
 end
 
 
-list = LinkedList.new( Node.new( "Cat", "Something that meows", nil ))
+word = Hashtable.new
 
+word.insert( "cat" )
+word.insert( "kangaroo" )
+word.insert( "b" )
+word.insert( "dear" )
+word.insert( "car" )
+word.insert( "e" )
+word.insert( "z" )
 
-list.add_node("Bear", "A bear")
-list.add_node("Fish", "A swimmer")
-list.add_node("Garden", "A grower")
-list.add_node("Shirt", "A shirt")
-
-
-list.insert_node( "Dragon", "fire breather", 4 )
-
-
-list.find_node( 5 )
-
-list.reverse
+word.render_list
---
+++ linkedlist.rb
@@ -0,0 +1,166 @@
+
+Node = Struct.new( :word, :definition, :next )
+
+class LinkedList
+
+	attr_accessor :head, :last
+
+	def initialize( first_node = nil )
+
+		@head = first_node
+		@last = first_node
+
+	end
+
+	def add_first_node( word, definition )
+
+		@head = Node.new( data, definition, nil )
+		@last = @head
+
+	end
+
+
+
+	def add_node( word, definition )
+
+		if @head.nil?
+
+			add_first_node( word, definition )
+
+		else
+
+			new_node = Node.new( word, definition )
+
+			@last.next = new_node
+
+			@last = new_node
+
+		end
+
+		puts "Added node with value: #{word}: #{definition}"
+
+	end
+
+ # Big O is O(n) since the size of the linked list determines length to traverse the nodes
+	def find_node( index )
+
+
+		count = 0
+		current_node = @head
+
+		while count < index
+
+
+		raise "No node at index" if current_node.next == nil
+
+			print_node( current_node, count )
+
+			current_node = current_node.next
+			count += 1
+
+		end
+
+		puts "Found node at index #{count}"
+		puts "Word is #{current_node.word}"
+		puts "Definition is #{current_node.definition}"
+
+	end
+
+
+	def print_node( node, index )
+
+		puts "Currently at index #{index}: #{node.word}"
+
+	end
+
+
+	# this is O(1) assuming we've already foung the node since we're only moving references
+	def insert_node( word, definition, index )
+
+		count = 0
+		current_node = @head
+		last_node = @head
+
+
+		while count < index
+
+			last_node = current_node
+			current_node = current_node.next
+
+			count += 1
+
+		end
+
+		if last_node.next == nil
+
+			add_node( word, definition )
+
+		else
+
+			new_node = Node.new( word, definition )
+			new_node.next = current_node
+			last_node.next = new_node
+
+			puts "Inserted #{new_node.word} at index: #{count}"
+
+		end
+
+	end
+
+
+	def remove_node
+
+		return nil if @head.nil?
+
+		node = @head
+
+		@head = @head.next
+
+		return node
+
+	end
+
+
+
+	# O(n) - creates new nodes in this method - time will be linear
+	def reverse
+
+		return if @head.nil?
+
+		temp = self.remove_node
+		temp.next = nil
+		@last = temp
+
+		until @head.nil?
+
+			node = self.remove_node
+			node.next = temp
+			temp = node
+
+		end
+
+		@head = temp
+
+	end
+
+
+end
+
+=begin
+list = LinkedList.new( Node.new( "Cat", "Something that meows", nil ))
+
+
+list.add_node("Bear", "A bear")
+list.add_node("Fish", "A swimmer")
+list.add_node("Garden", "A grower")
+list.add_node("Shirt", "A shirt")
+
+
+list.insert_node( "Dragon", "fire breather", 4 )
+
+
+list.find_node( 5 )
+
+list.reverse
+
+=end
