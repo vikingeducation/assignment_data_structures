3cd1459296434ed92693bfebc76e7507bbb4b45a
John Sawyer <sawyer@sawyermerchant.com>
2016-11-08 15:03:31 -0500

    Started linked list

--- linked_list.rb
+++ linked_list.rb
@@ -1,7 +1,27 @@
 class LinkedList
+  attr_accessor = :list_end
 
+  def initialize(entry = nil)
+    @entry = entry
+    @list_end = entry.word
+  end
 
+  def add_entry(entry)
+    list_end.point = entry.word
+  end
+
+  def read
+  end
+
+  def insert
+  end
 
 end
 
 Entry = Struct.new(:word, :definition, :pointer)
+
+
+s = Entry.new("cat", "dog that barks", nil)
+
+dictionary = LinkedList.new
+dictionary.add_entry(s)
