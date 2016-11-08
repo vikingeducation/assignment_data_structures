2dbb714e6846b345f4243cf5a9534672835f9449
Jessica G <gillan.jessica@gmail.com>
2016-11-08 11:03:20 -0700

    start stack

---
+++ stack_queue.rb
@@ -0,0 +1,46 @@
+class Stack
+  # push
+  # pop
+  # peek
+  # empty
+
+  # Basic data: Array
+  # LIFO, stack of plates
+  # only indexing, length
+  # No enumerable
+
+  def initialize(arr)
+    @stack = Array.new( arr.length * 4 )
+    set_stack
+    @last = 0
+  end
+
+  def set_stack
+    for i in (0..arr.length - 1)
+      push(arr[i])
+    end
+  end
+
+  def push(item)
+    stack[last] = item
+    last += 1
+  end
+
+  def pop
+    item = stack[stack.length - 1]
+    stack = stack[0..(stack.length - 2)]
+    item
+  end
+
+  # Top item only
+  def peek
+
+  end
+
+  def empty?
+  end
+
+  private
+  attr_reader :stack
+  attr_accessor :last
+end
