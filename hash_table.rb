0de971a292ecbc50edd4e5fe754f61e47b20b6b1
Dexter Ford <dexter.ford.77@gmail.com>
2016-11-08 16:55:04 -0600

    Working on render for hash

--- hash_table.rb
+++ hash_table.rb
@@ -1,3 +1,4 @@
+require_relative 'linked_list'
 
 class HashTable
 
@@ -9,5 +10,21 @@ class HashTable
     key[0].ord - 97
   end
 
-end
+  def insert(word, definition)
+    bucket_index = hash(word)
+    unless @buckets[bucket_index]
+      list = LinkedList.new
+      list.add_node(word, definition)
+      @buckets[bucket_index] = list
+    else
+      @buckets[bucket_index].add_node(word, definition)
+    end
+  end
 
+  def render_list
+    ("A".."Z").each_with_index do |letter, index|
+      puts "#{letter}: #{}"
+    end
+  end
+
+end
