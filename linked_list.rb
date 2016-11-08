4c8553f00fe2dc2afbd05f91902f6fdbd86acbbb
Jake Tingley <jaket32@gmail.com>
2016-11-08 17:55:07 -0500

    linked list work

--- linked_list.rb
+++ linked_list.rb
@@ -1,27 +1,69 @@
+Node = Struct.new(:data, :next)
+
 class LinkedList
-  attr_accessor = :list_end
+  
+  attr_accessor :head, :last
 
-  def initialize(entry = nil)
-    @entry = entry
-    @list_end = entry.word
+  def initialize(first_node=nil)
+    @head = first_node
+    @last = first_node
   end
 
-  def add_entry(entry)
-    list_end.point = entry.word
+  def add_first_node(data)
+    @head = Node.new(data, nil)
+    @last = @head
   end
 
-  def read
+  def add_node(data)
+    #add new entry
+    #change pointer of previous to point to new end
+    if @head.nil?
+      add_first_node(data)
+    else
+      new_node = Node.new(data)
+      @last.next = new_node
+      @last = new_node
+    end
+
+    puts "Added node with value: #{data}"
   end
 
-  def insert
+  def find_node(index)
+    counter = 0
+    current_node = @head
+    while counter < index
+      current_node = current_node.next
+      puts "At index #{index} current value is #{current_node.data}"
+      counter += 1
+    end
+
+    puts "Found node at index #{index} with current value #{current_node.data}"
+    current_node
+  end
+
+
+  def insert(data, index)
+    counter = 0
+    current_node = @head
+    while counter < index
+      current_node = current_node.next
+     # puts "At index #{index} current value is #{current_node.data}"
+      counter += 1
+    end
+    insert_node = Node.new(data)
+    insert#new node points to current_node.next
+    current_node.pointer = Node.new(data)
   end
 
 end
 
-Entry = Struct.new(:word, :definition, :pointer)
 
 
-s = Entry.new("cat", "dog that barks", nil)
+#s = Entry.new("cat", nil)
 
-dictionary = LinkedList.new
-dictionary.add_entry(s)
+l1 = LinkedList.new(Node.new("first_node", nil))
+l1.find_node(0)
+l2 = LinkedList.new
+l2.add_node("another first node")
+p l2.head
+l1.add_node("second node")
