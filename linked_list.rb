32f8733835cb818c5a9c5026ee69d0a4fb081897
Catherine Kwak <khopsickle@users.noreply.github.com>
2016-11-08 12:40:48 -0700

    Add a few methods to linkedlist

--- linked_list.rb
+++ linked_list.rb
@@ -1,44 +1,57 @@
-# nodes(Structs?)
-  # data, pointer
-
-# to access specific data
-  # start at first node
-  # crawl using pointers until
-    # node data == input data
-
-# to add a node(data, index)
-  # state big-0 time
-  # crawl until reached index
-  # point previous node to input
-  # point input to next node
-
-# to remove a node(index || data)
-  # crawl until reached index || data
-  # point previous data to the next data
-  # set pointer to nil
-
-# to update a node(index, new_data)
-  # crawl until reached index
-  # set data to new_data
-
-# to reverse the list
-  # state big-0 time
-  # crawl to end
-  # build new linked list, adding last node to start
-  # delete last node
-  # until list is empty  
-    # crawl to end
-    # add to new list
-    # delete....
-
-  #loop
-    # crawl until pointer is nil or the next node points to the current node
-    # if pointer points to previous_node
-      # current_node = previous_node
-      # current points to double_previous
-    # point this node to previous
-
 Node = Struct.new(:data, :pointer)
+
 class LinkedList
 
+  attr_reader :node_list
+
+  def initialize(node = nil)
+    @first_node = node
+    @current_node = @first_node
+  end
+
+  def node_list
+    index = 0
+    until end_of_list
+      puts "Node #{index}: #{@current_node.data}"
+      crawl
+      index += 1
+    end
+    puts "Node #{index}: #{@current_node.data}"
+  end
+
+  def end_of_list
+    @current_node.pointer == nil
+  end
+
+  def reset_current_node
+    @current_node = @first_node
+  end
+
+  def crawl
+    @current_node = @current_node.pointer
+  end
+
+  def add(node)
+    reset_current_node
+    if @current_node == nil
+      @current_node = node
+    else
+      until @current_node.pointer == nil
+        crawl
+      end
+      @current_node.pointer = node
+    end
+  end
+
+  def read(index)
+
+  end
 end
+
+l = LinkedList.new
+
+l.add(Node.new("apple",nil))
+l.add(Node.new("banana",nil))
+l.add(Node.new("carrot",nil))
+
+l.node_list
