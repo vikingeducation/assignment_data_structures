d1367180e3205576b4b12d5dcafa0c8b32d91689
Catherine Kwak <khopsickle@users.noreply.github.com>
2016-11-08 11:45:24 -0700

    Finish stack

--- stack.rb
+++ stack.rb
@@ -4,6 +4,8 @@
 # loop, while, times, for
 # do not use each, map
 # do not use delete, at?, empty?
+# add an empty? helper
+# verify you can reverse a string
 
 # stack:
   # pop
@@ -11,11 +13,11 @@
   # peek the start (can implement other elements)
 
 
-class Stack < Array
+class Stack
   attr_reader :array
 
   def initialize(array)
-    @array = array  
+    @array = array
   end
 
   def pop
@@ -47,11 +49,38 @@ class Stack < Array
   def peek(index = 0)
     @array[index]
   end
+
+  def empty?
+    @array.length == 0
+  end
+
 end
 
-s = Stack.new([1,2,3])
-puts s.array
-s.push(4)
-puts s.array
-puts s.peek
-puts s.peek(2)
+def reverse(string)
+  # string = "hello"
+
+  # iterate from end of string
+  #   s.push(from end of string)
+  #   [o]
+  #   s.push()
+  #   [o, l]
+
+  char = string.length - 1
+  reversed_array = Stack.new([])
+
+  while char >= 0
+    reversed_array.push(string[char])
+    char -= 1
+  end
+
+  reversed_array.array.join("")
+end
+
+# s = Stack.new([1,2,3])
+# puts s.array
+# s.push(4)
+# puts s.array
+# puts s.peek
+# puts s.peek(2)
+
+puts reverse("hello")
