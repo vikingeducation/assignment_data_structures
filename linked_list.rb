93ddc67a2c18bedd2d01a1fb438a7c4a4e3c64f3
ttscott2 <timothytscott3@gmail.com>
2016-12-09 20:10:22 -0600

    add linked list methods

--- linked_list.rb
+++ linked_list.rb
@@ -1,2 +1,17 @@
+Node = Struct.new(:word, :definition, :next)
+
 class LinkedList
+  attr_reader :head
+
+  def initialize(first_node = nil)
+    raise "Please only enter nodes an an argument" unless first_node.is_a?(Node) ||
+      first_node.nil?
+      @head = first_node
+      @tail = first_node
+      @length = 0
+      @counter = 0
+      end
+  end
+
+
 end
