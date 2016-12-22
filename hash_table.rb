186de09a28f2ed27302da6d432d405e563187011
Visiona <darbis@gmail.com>
2016-12-22 00:23:58 +0000

    refactored one method in hash_table and created new render method

--- hash_table.rb
+++ hash_table.rb
@@ -16,37 +16,51 @@ class HashTable
 
   def insert(word, definition = nil)
     idx = self.hash(word)
-    if @buckets[idx] == nil
-      @buckets[idx] = LinkedList.new
-      @buckets[idx].add_first_node(word, definition)
-    else
-      @buckets[idx].add_node(word, definition)
-    end
+    @buckets[idx] = LinkedList.new if @buckets[idx] == nil
+    @buckets[idx].add_node(word, definition)
   end
 
-  def render_bucket(bucket)
-    if bucket != nil
-      current_node = bucket.head
-      puts " " * 10 + "#{current_node.word}"
-      loop do
-        current_node = current_node.next
-        break if current_node == nil
-        puts " " * 10 + "#{current_node.word}"
-      end
-    end
-  end
+  # def render_bucket(bucket)
+  #   if bucket != nil
+  #     current_node = bucket.head
+  #     puts " " * 10 + "#{current_node.word}"
+  #     loop do
+  #       current_node = current_node.next
+  #       break if current_node == nil
+  #       puts " " * 10 + "#{current_node.word}"
+  #     end
+  #   end
+  # end
 
   def render_list
-    puts "Here is the printout of the dictionary HasTable"
-    puts
-    26.times do |idx|
+    @buckets.size.times do |idx|
+      counter = 0
       if @buckets[idx] != nil
-        chosen_bucket = @buckets[idx]
-        puts "The bucket name is #{ALPHABET[idx]} and it's content is:  "
-        render_bucket(chosen_bucket)
+        current_node = @buckets[idx].head
+        print "#{current_node.word}(#{current_node.def})".ljust(13)
+        while !current_node.next.nil?
+          current_node = current_node.next
+          print "#{current_node.word}(#{current_node.def})".ljust(13)
+          counter += 1
+        end
+        puts
       end
     end
   end
+
+
+
+  # def render_list
+  #   puts "Here is the printout of the dictionary HasTable"
+  #   puts
+  #   26.times do |idx|
+  #     if @buckets[idx] != nil
+  #       chosen_bucket = @buckets[idx]
+  #       puts "The bucket name is #{ALPHABET[idx]} and it's content is:  "
+  #       render_bucket(chosen_bucket)
+  #     end
+  #   end
+  # end
 
   def define(searched_word)
     idx = self.hash(searched_word)
