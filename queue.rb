dbd8ba54418a329601f614cea3ac5128a9ba8119
Chad Lucas <chad.lucas.19@gmail.com>
2017-03-19 01:10:14 -0500

    add dequeue method

--- queue.rb
+++ queue.rb
@@ -1,11 +1,30 @@
 class Queue
 
-	attr_accessor :queue, :head
-	
+	attr_accessor :queue
+
 	def initialize(queue = [])
 		@queue = queue
-		@head = 0
+		
+	end
+
+	def dequeue
+		#take first element from array
+		popped = @queue[0]
+    #sets up empty array
+		new_array = []
+    #starts counter at 1
+		index = 1
+		#iterates through array and 
+		while index < (@queue.length)
+			new_array[index] = @queue[index]
+			index += 1
+		end
+		#returns dequeue element and updated array
+		@queue = new_array
+		popped
+		puts @queue
+
 	end
 	
-
 end
+
