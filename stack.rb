fc5770814b8074d768125afdbf60502d40528bae
Chad Lucas <chad.lucas.19@gmail.com>
2017-03-18 23:21:29 -0500

    add push method

--- stack.rb
+++ stack.rb
@@ -1,26 +1,49 @@
 class Stack
 	attr_accessor :stack
 
+	#sets up stack
 	def initialize(stack = [])
+
 		@stack = stack
 	end
 
 	def pop
-			
-
-			popped = @stack[@stack.length-1]
-			
-      popped
-
+		  #outputs error message if array length is zero
+			return "empty" if empty?
       
+      #removes last element in array
+			popped = @stack[@stack.length-1]
+
+			#returns last element
+      popped
+      
+      #setup empty array and counter
+      new_array = []
+      index = 0
+      
+      #crawls through array and places each element into new array
+      while index < (@stack.length - 1)
+
+        new_array[index] = @stack[index]
+        index += 1
+      end
+
+       #returns updated array and popped element
+       @stack = new_array
+       popped
+  end
+
+  def push(element)
+  	#pushes element onto end of array
+    @stack[@stack.length] = element
+
+  end
+
+	def empty?
+    #checks if array is empty
+		@stack.length == 0
 
 	end
-  
 
 end
-
-
-plates = Stack.new(stack = [1,2,3,4])
-
-puts plates.pop.inspect
 
