337e59213767c1f51a4f4e5eda22d037bc375dc0
Chad Lucas <chad.lucas.19@gmail.com>
2017-04-17 17:21:18 -0500

    add myqueu class

--- hash_table.rb
+++ hash_table.rb
@@ -1,3 +1,5 @@
+require 'link_list'
+
 class HashTable
 	#sets up empty array
 	def initialize
@@ -5,14 +7,28 @@ class HashTable
   end
 
   def hash(word)
+    #gets first letter of word and gets numerical value
   	puts word.chars.first.upcase.ord - 65
   end
 
   def insert(word, definition)
-   index = hash(word)
+    #calls hash method and assigns it to index
+    index = hash(word)
+    
+  	@buckets[index] = LinkedList.new if @buckets[index].nil?
 
-  	@buckets[index] = 
+    @buckets[index].append(word, definition)
   end
+
+  def bucket_sizes
+    0.upto(25) do |i|
+      print "bucket #{i}:"
+      puts @buckets[i] ? @buckets[i].size : "empty"
+    end
+    
+  end
+
+
 
 
 end
