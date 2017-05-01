836b25e867f915a1190adbeef109d99a5995168d
Brent Guffens <brentguf@users.noreply.github.com>
2017-05-01 14:04:35 +0200

    stack finish

---
+++ stack.rb
@@ -0,0 +1,29 @@
+class Stack
+
+	def initialize 
+    @stack = []
+	end
+
+  def push(item)
+    @stack[length] = item
+  end
+
+  def pop
+    stack_length = @stack.length
+
+		new_stack = []
+		i = 0
+		while i < @stack.length - 1
+		  new_stack[i] = stack[i]
+		  i += 1
+    end
+
+    @stack = new_stack
+
+  end
+
+  def peek
+    @stack[0]
+  end
+
+end
